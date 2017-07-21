ba70c1097a362abfc7c55231a25fa698e1ab420e
Gabriel Kaam <gabriel@kaam.fr>
2017-07-21 08:44:46 +0200

    Removes js script Update css to remove use of 3D transforms Update PHP

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($){
-	jQuery(":not('body.single-product')").find('ul.products li.pif-has-gallery a:first-child' ).hover( function() {
-		jQuery( this ).children( '.wp-post-image' ).removeClass( 'fadeInDown' ).addClass( 'animated fadeOutUp' );
-		jQuery( this ).children( '.secondary-image' ).removeClass( 'fadeOutUp' ).addClass( 'animated fadeInDown' );
-	}, function() {
-		jQuery( this ).children( '.wp-post-image' ).removeClass( 'fadeOutUp' ).addClass( 'fadeInDown' );
-		jQuery( this ).children( '.secondary-image' ).removeClass( 'fadeInDown' ).addClass( 'fadeOutUp' );
-	});
-});
